966b7f91210c62198ff1ec0bfb1afe7985fce8be
jonniebigodes <jonniebigodes@hotmail.com>
2019-03-22 17:29:43 +0000

    first commit

---
+++ src/templates/blog-post.js
@@ -0,0 +1,96 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm, scale } from "../utils/typography"
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    const post = this.props.data.markdownRemark
+    const siteTitle = this.props.data.site.siteMetadata.title
+
+    const { previous, next } = this.props.pageContext
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title={post.frontmatter.title}
+          description={post.frontmatter.description || post.excerpt}
+        />
+        <Img
+          className="mainImage"
+          fluid={post.frontmatter.featuredImage.childImageSharp.fluid}
+        />
+        <div className="site-body">
+          <div className="grid grid-gutters grid-justifyCenter">
+            <div className="grid-cell">
+              <div className="blogTitle singleBlog">
+                <h1>{post.frontmatter.title}</h1>
+                <h6>{post.frontmatter.date}</h6>
+              </div>
+              <div
+                className="blogContent"
+                dangerouslySetInnerHTML={{ __html: post.html }}
+              />
+              <hr />
+              <ul
+                style={{
+                  display: `flex`,
+                  flexWrap: `wrap`,
+                  justifyContent: `space-between`,
+                  listStyle: `none`,
+                  padding: 0,
+                }}
+              >
+                <li>
+                  {previous && (
+                    <Link to={previous.fields.slug} rel="prev">
+                      ← {previous.frontmatter.title}
+                    </Link>
+                  )}
+                </li>
+                <li>
+                  {next && (
+                    <Link to={next.fields.slug} rel="next">
+                      {next.frontmatter.title} →
+                    </Link>
+                  )}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD,YYYY")
+        featuredImage {
+          childImageSharp {
+            fluid(maxWidth: 1500) {
+              ...GatsbyImageSharpFluid_noBase64
+            }
+          }
+        }
+      }
+    }
+  }
+`
